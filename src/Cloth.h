31b9beff0165223e1c9a1bbaa86759bf91c4628a
Keven Villeneuve <keven.villeneuve@gmail.com>
2017-04-17 21:18:25 -0400

    inline Cloth getters

--- src/Cloth.h
+++ src/Cloth.h
@@ -13,11 +13,11 @@ public:
 	void clear();
 	void generate(int width, int height, float resolution);
 
-	std::vector<Spring>& getSprings() { return m_Springs; }
-	std::vector<Particle>& getParticles() { return m_Particles; }
-	int getWidth() { return m_Width; }
-	int getHeight() { return m_Height; }
-	float getResolution() { return m_Resolution; }
+	inline std::vector<Spring>& getSprings() { return m_Springs; }
+	inline std::vector<Particle>& getParticles() { return m_Particles; }
+	inline int getWidth() { return m_Width; }
+	inline int getHeight() { return m_Height; }
+	inline float getResolution() { return m_Resolution; }
 
 private:
 	std::vector<Spring> m_Springs;
