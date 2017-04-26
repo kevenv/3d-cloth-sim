a9a1958bf55c53776e15ba15992753059a969494
Keven Villeneuve <keven.villeneuve@gmail.com>
2017-04-25 21:29:01 -0400

    Cleanup main

--- src/main.cpp
+++ src/main.cpp
@@ -118,14 +118,14 @@ int main()
 	driver->setTextureCreationFlag(video::ETCF_ALWAYS_32_BIT, true);
 
 	// add camera
-	//scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0, 100.0f, 1.2f);
-	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeMaya(0, 100.0f, 1.2f);
+	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0, 100.0f, 1.2f);
+	//scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeMaya(0, 100.0f, 1.2f);
 	camera->setPosition(core::vector3df(0,750,-879));
 	camera->setTarget(core::vector3df(13, 105, 80));
 	camera->setFarValue(42000.0f);
 
 	// disable mouse cursor
-	//device->getCursorControl()->setVisible(false);
+	device->getCursorControl()->setVisible(false);
 
 	// floor
 	core::dimension2df tileSize(256,256);
@@ -137,16 +137,6 @@ int main()
 	floorNode->setMaterialType(video::EMT_SOLID);
 	floorNode->setMaterialFlag(video::EMF_LIGHTING, false);
 
-	// cube
-	/*
-	scene::IMesh* cubeMesh = smgr->getGeometryCreator()->createCubeMesh(core::vector3df(1.0f, 1.0f, 1.0f));
-	scene::IMeshSceneNode* cubeNode = smgr->addMeshSceneNode(cubeMesh, 0, -1, core::vector3df(0, 0, 0));
-	cubeNode->setScale(core::vector3df(250, 250, 250));
-	cubeNode->setPosition(core::vector3df(0,0,0));
-	cubeNode->setMaterialType(video::EMT_SOLID);
-	cubeNode->setMaterialFlag(video::EMF_LIGHTING, false);
-	*/
-
 	// create skydome
 	driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, false);
 	scene::ISceneNode* skydome=smgr->addSkyDomeSceneNode(driver->getTexture("../irrlicht/media/skydome.jpg"),16,8,0.95f,2.0f);
@@ -157,7 +147,7 @@ int main()
 	axisNode->setScale(core::vector3df(300, 300, 300));
 	axisNode->setPosition(core::vector3df(0,50,0));
 	
-	// light is just for nice effects
+	// light
 	scene::ILightSceneNode *node = smgr->addLightSceneNode(0, core::vector3df(0, 100, 0),
 		video::SColorf(1.0f, 0.6f, 0.7f, 1.0f), 500.0f);
 	if (node)
@@ -197,6 +187,7 @@ int main()
 	MyEventReceiver receiver(device);
 	device->setEventReceiver(&receiver);
 
+	// 3D picking
 	scene::ISceneCollisionManager* collMan = smgr->getSceneCollisionManager();
 	Particle* selectedParticle = NULL;
 	core::vector2di clickPos;
@@ -294,7 +285,6 @@ int main()
 	clothRenderer.close();
 	axisNode->drop();
 	floorMesh->drop();
-	//cubeMesh->drop();
 	device->drop();
 	
 	return 0;
